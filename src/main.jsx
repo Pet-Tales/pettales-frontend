67a16d756dcdc9bf150b52a0d07f6ab0a4d62215
Gallner131 <72497136+Gallner131@users.noreply.github.com>
2025-10-20 14:05:41 +0100

    Add HelmetProvider for SEO

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
 import "./i18n"; // Initialize i18n
@@ -9,8 +10,10 @@ import "./assets/index.css";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>
   // </StrictMode>
 );
